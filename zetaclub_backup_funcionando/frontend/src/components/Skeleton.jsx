c2fb4d650734bdbf91ddd56120f7630f6589fd5b
Lucas Sanches <lucas8sanches20@gmail.com>
2026-05-30 11:16:21 -0300

    Removido backup do repositório

--- zetaclub_backup_funcionando/frontend/src/components/Skeleton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Skeleton = ({ className }) => {
-  return (
-    <div className={`animate-pulse bg-slate-700 rounded ${className}`}></div>
-  );
-};
-
-export default Skeleton;
