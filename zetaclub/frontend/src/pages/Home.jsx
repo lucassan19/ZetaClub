4acf73886aa54b3862230cb36176d31ff1f01cd2
Lucas Sanches <lucas8sanches20@gmail.com>
2026-07-21 13:05:03 -0300

    Titulo e frase no home.jsx alterados

--- zetaclub/frontend/src/pages/Home.jsx
+++ zetaclub/frontend/src/pages/Home.jsx
@@ -123,9 +123,9 @@ const Home = () => {
               A nova era do <br />
               <span className="text-primary-500">porno</span> exclusivo.
             </h1>
-            <p className="text-slate-400 text-lg mb-8 leading-relaxed font-medium">
-              {/* Acesse os melhores vídeos pornos com qualidade Ultra HD, navegação inteligente e experiência imersiva feita para você. */}
-            </p>
+            {/* <p className="text-slate-400 text-lg mb-8 leading-relaxed font-medium">
+              Acesse os melhores vídeos pornos com qualidade Ultra HD, navegação inteligente e experiência imersiva feita para você.
+            </p> */}
             <div className="flex flex-wrap gap-4">
               {/* <button className="px-8 py-4 bg-primary-600 hover:bg-primary-500 text-white rounded-2xl font-bold transition-all shadow-lg shadow-primary-900/20 hover:scale-105 active:scale-95">
                 Começar a assistir
